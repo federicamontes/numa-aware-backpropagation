1673d2af3c273229e0129b5fd29ec62335f24bcb
federica <federica.montesano42@gmail.com>
2026-01-20 16:12:14 +0100

    added utiliy function to pin core on numa node

--- backpropagation-parallel/include/utils.h
+++ backpropagation-parallel/include/utils.h
@@ -2,6 +2,7 @@
 #define UTILS_H
 
 #include <stddef.h>
+#include <sched.h>
 
 extern int seed; // Declare as extern so it can be accessed elsewhere
 
--- backpropagation-parallel/src/utils.c
+++ backpropagation-parallel/src/utils.c
@@ -1,5 +1,9 @@
+#define _GNU_SOURCE  
 #include "utils.h"
 #include <stdlib.h>
+#include <numa.h>
+#include <unistd.h>
+
 
 int seed; // Actual definition
 
@@ -20,4 +24,40 @@ void shuffle(int* arr, size_t n) {
             arr[i] = t;
         }
     }
+}
+
+
+/**
+ * Pins the calling process to all CPU cores associated with a specific NUMA node.
+ * @param node_id: The ID of the NUMA node (0, 1, etc.)
+ * @return: 0 on success, -1 on failure
+ */
+int pin_to_numa_node(int node_id) {
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+
+    // Get the bitmask of CPUs for this NUMA node from libnuma
+    struct bitmask* cpus = numa_allocate_cpumask();
+    if (numa_node_to_cpus(node_id, cpus) != 0) {
+        perror("numa_node_to_cpus");
+        numa_free_cpumask(cpus);
+        return -1;
+    }
+
+    // Transfer the bits from the bitmask to the cpu_set_t
+    for (unsigned int i = 0; i < cpus->size; i++) {
+        if (numa_bitmask_isbitset(cpus, i)) {
+            CPU_SET(i, &mask);
+        }
+    }
+
+    // Apply the affinity to the calling process (pid 0)
+    if (sched_setaffinity(0, sizeof(cpu_set_t), &mask) == -1) {
+        perror("sched_setaffinity");
+        numa_free_cpumask(cpus);
+        return -1;
+    }
+
+    numa_free_cpumask(cpus);
+    return 0;
 }
