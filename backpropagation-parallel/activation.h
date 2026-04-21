cd853f57df9d97de2107bf0830649fddbc3ddae2
federica <federica.montesano42@gmail.com>
2026-04-21 10:58:43 +0200

    changed signature for compilation purposes

--- backpropagation-parallel/activation.h
+++ backpropagation-parallel/activation.h
@@ -4,20 +4,20 @@
 
 
 // Sigmoid activation function
-double sigmoid(double x){
+static inline double sigmoid(double x){
     double ans = (double)1/(double)(1 + exp(-x));
     return ans;
 }
 
 
 // Derivative of Sigmoid activation function
-double sigmoid_d(double x){
+static inline double sigmoid_d(double x){
     return sigmoid(x)*(1-sigmoid(x));
 }
 
 
 // ReLU activation function
-double relu(double x){
+static inline double relu(double x){
     if(x < 0.0){
         return 0.0;
     }
@@ -26,7 +26,7 @@ double relu(double x){
 
 
 // Derivative of ReLU activation function
-double relu_d(double x){
+static inline double relu_d(double x){
     if(x < 0.0){
         return 0.0;
     }
@@ -34,7 +34,7 @@ double relu_d(double x){
 }
 
 // Derivative of tanh activation function
-double tanh_d(double x){
+static inline double tanh_d(double x){
     double res = 1.0 - tanh(x)*tanh(x);
     return res;
 }
