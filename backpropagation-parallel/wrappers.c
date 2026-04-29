1ac172436c9a30e14341799adbaf800e79414d76
federica <federica.montesano42@gmail.com>
2026-04-29 09:15:35 +0200

    added wrapper function of forward propagation: this function needs to be wrapped because it is called by both workers and parent process, and it needs to be called with two different representations of the neural network, once associated with the process' private network, once with the shared network

--- backpropagation-parallel/wrappers.c
+++ backpropagation-parallel/wrappers.c
@@ -3,6 +3,7 @@
 
 #include <numa.h>
 #include "neural_network.h"
+#include <unistd.h> // Per getpid()
 
 // Dichiarazione delle funzioni reali (servono per il fallback)
 extern struct NeuralNet* __real_newNet(int n_layers, int n_neurons_per_layer[]);
@@ -15,6 +16,9 @@ extern double* __real_model_train(struct NeuralNet* nn, double** X, double** y, 
 extern double* __real_model_test(struct NeuralNet* nn, double** X, double** y, 
                                  double* y_t, char* act, char* loss);
 
+extern void __real_forward_propagation(struct NeuralNet* nn, char* activation_fun, char* loss);
+
+extern unsigned long base_address;
 
 // Wrapper for allocation
 struct NeuralNet* __wrap_newNet(int n_layers, int n_neurons_per_layer[]) {
@@ -70,6 +74,29 @@ double* __wrap_model_test(struct NeuralNet* nn_numa, double** X, double** y,
 
     // Chiamiamo la funzione reale passandole l'indirizzo della struct standard
     return __real_model_test(&standard_nn, X, y, y_t, act, loss);
+}
+
+// Wrapper per forward_propagation
+void __wrap_forward_propagation(struct NeuralNet* nn_or_ws, char* activation_fun, char* loss) {
+
+    // In questo contesto, nn_or_ws è SEMPRE un NNWorkerWorkspace*
+    NNWorkerWorkspace* ws = (NNWorkerWorkspace*)nn_or_ws;
+
+    // Creiamo l'adapter locale per parlare con la funzione sequenziale
+    struct NeuralNet adapter_nn;
+    
+    // Mappiamo i dati: Pesi globali + Buffer privati del worker
+    adapter_nn.n_layers = ws->shared_nn->n_layers;
+    adapter_nn.n_neurons_per_layer = ws->shared_nn->n_neurons_per_layer;
+    adapter_nn.w = ws->shared_nn->w;
+    adapter_nn.b = ws->shared_nn->b;
+    
+    adapter_nn.in = ws->in;
+    adapter_nn.out = ws->out;
+    adapter_nn.targets = ws->targets;
+
+    // Eseguiamo la funzione reale
+    __real_forward_propagation(&adapter_nn, activation_fun, loss);
 }
 
 
